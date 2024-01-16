faf5353eda27fb5a9512e48b8ac4382cf47b7bda
FoyerD <amitfoye@post.bgu.ac.il>
2024-01-16 15:23:42 +0200

    added clone, getters, step, hasOrdersLeft and canTakeOrder, started decreaseCoolDown

--- src/Volunteer.cpp
+++ src/Volunteer.cpp
@@ -1,16 +1,45 @@
 #include "Volunteer.h"
 
 //Volunteer class
-Volunteer::Volunteer(int id, const string& name) : id(id), name(name){}
+Volunteer::Volunteer(int id, const string& name):
+                    id(id),
+                    name(name)
+                    {completedOrderId = NO_ORDER;
+                    activeOrderId = NO_ORDER;}
 
-int Volunteer::getId() const{return this->id;}
-const string& Volunteer::getName() const{return this->name;}
-int Volunteer::getActiveOrderId() const{return this->activeOrderId;}
-int Volunteer::getCompletedOrderId() const{return this->completedOrderId;}
+int Volunteer::getId() const{return id;}
+const string& Volunteer::getName() const{return name;}
+int Volunteer::getActiveOrderId() const{return activeOrderId;}
+int Volunteer::getCompletedOrderId() const{return completedOrderId;}
 
 
-bool Volunteer::isBusy() const{return this->activeOrderId==NO_ORDER;}
+bool Volunteer::isBusy() const{return !(activeOrderId == NO_ORDER);}
+
+
 
 
 //CollectorVolunteer class
-CollectorVolunteer::CollectorVolunteer(int id, string name, int coolDown) : Volunteer::Volunteer(id,name), coolDown(coolDown){}
+CollectorVolunteer::CollectorVolunteer(int id, string name, int coolDown):
+                                        Volunteer::Volunteer(id,name),
+                                        coolDown(coolDown)
+                                        {timeLeft = coolDown;}
+CollectorVolunteer* CollectorVolunteer::clone() const {
+    return new CollectorVolunteer(getId(), getName(), coolDown);
+}
+
+int CollectorVolunteer::getCoolDown() const {return coolDown;}
+int CollectorVolunteer::getTimeLeft() const {return timeLeft;}
+
+bool CollectorVolunteer::hasOrdersLeft() const {return true;}
+bool CollectorVolunteer::canTakeOrder(const Order& order) const {
+    return order.getStatus() == OrderStatus::PENDING && hasOrdersLeft();
+}
+void CollectorVolunteer::step(){
+    if(decreaseCoolDown()){
+         completedOrderId = activeOrderId;
+         activeOrderId = NO_ORDER;
+    }
+}
+void CollectorVolunteer::decreaseCoolDown(){
+    
+}
