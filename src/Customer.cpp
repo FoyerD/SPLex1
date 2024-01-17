d553bb17cfa3b11007592fe1f62e9a7c510500bb
eitandub22 <eitandu@post.bgu.ac.il>
2024-01-17 14:59:30 +0200

    added comments

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -1,6 +1,14 @@
 #include "Customer.h"
+//Customer class
 
+/**
+ * @param id - id of the new volunteer, given from counter
+ * @param name - name of the volunteer
+ * @param locationDistance - the distance from the warehouse to th customer
+ * @param maxOrders - the max number of orders the client can make
+*/
 Customer::Customer(int id, const string &name, int locationDistance, int maxOrders): id(id), name(name), locationDistance(locationDistance), maxOrders(maxOrders){}
+
 
 const string &Customer::getName() const{
     return name;
@@ -12,27 +20,34 @@ int Customer::getCustomerDistance() const{
     return locationDistance;
 }
 
-//Returns maxOrders
 int Customer::getMaxOrders() const{
     return maxOrders;
 } 
 
-//Returns num of orders the customer has made so far
+/**
+ * @returns - the number of orders the customer has made so far
+*/
 int Customer::getNumOrders() const{
     return ordersId.size();
 } 
 
- //Returns true if the customer didn't reach max orders
+/**
+ * @returns - true if the customer didn't reach max orders else, false
+*/
 bool Customer::canMakeOrder() const{
     return (getNumOrders() < maxOrders) ? true : false;
 }
 
-//Returns all orders id's in a vector
+/**
+ * @returns - all orders id's in a vector
+*/
 const vector<int> &Customer::getOrders() const{
     return ordersId;
 }
 
-///return OrderId if order was added successfully, -1 otherwise
+/**
+ * @returns - OrderId if order was added successfully, -1 otherwise
+*/
 int Customer::addOrder(int orderId){
     if(canMakeOrder()){
         ordersId.push_back(orderId);
@@ -41,17 +56,34 @@ int Customer::addOrder(int orderId){
     else return -1;
 } 
 
-
+//SoldierCustomer class
+/**
+ * @param id - id of the new volunteer, given from counter
+ * @param name - name of the volunteer
+ * @param locationDistance - the distance from the warehouse to th customer
+ * @param maxOrders - the max number of orders the client can make
+*/
 SoldierCustomer::SoldierCustomer(int id, string name, int locationDistance, int maxOrders): Customer(id,name,locationDistance,maxOrders){}
 
-//return a SoldierCustomer object that is a clone to the current SoldierCustomer
+/**
+ * @returns - a SoldierCustomer object that is a clone to the current SoldierCustomer
+*/
 SoldierCustomer* SoldierCustomer::clone() const {
    return new SoldierCustomer(getId(), getName(), getCustomerDistance(), getMaxOrders());
 }
 
+//CivilianCustomer class
+/**
+ * @param id - id of the new volunteer, given from counter
+ * @param name - name of the volunteer
+ * @param locationDistance - the distance from the warehouse to th customer
+ * @param maxOrders - the max number of orders the client can make
+*/
 CivilianCustomer::CivilianCustomer(int id, string name, int locationDistance, int maxOrders): Customer(id,name,locationDistance,maxOrders){}
 
-//return a CivilianCustomer object that is a clone to the current CivilianCustomer
+/**
+ * @returns - a CivilianCustomer object that is a clone to the current CivilianCustomer
+*/
 CivilianCustomer* CivilianCustomer::clone() const {
     return new CivilianCustomer(getId(), getName(), getCustomerDistance(), getMaxOrders());
 }
