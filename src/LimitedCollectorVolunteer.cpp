09da6aa668429bd858bae83e1465858691a4459a
FoyerD <amitfoye@post.bgu.ac.il>
2024-01-18 11:55:35 +0200

    finished limitedCollectorVolunteer class

---
+++ src/LimitedCollectorVolunteer.cpp
@@ -0,0 +1,57 @@
+#include "Volunteer.h"
+
+//LimitedCollectorVolunteer
+/**
+ * @param id - id of the new volunteer, given from counter
+ * @param name - name of the volunteer
+ * @param coolDown - process time of 1 order
+ * @param maxOrders - number of orders instance can take before being deletet
+*/
+LimitedCollectorVolunteer::LimitedCollectorVolunteer(int id, string name, int coolDown, int maxOrders):
+                                                    CollectorVolunteer::CollectorVolunteer(id,name,coolDown),
+                                                    maxOrders(maxOrders),
+                                                    ordersLeft(maxOrders){}
+
+/**
+ * @returns - a new instance of volunteer with same parameters(not busy)
+*/
+LimitedCollectorVolunteer* LimitedCollectorVolunteer::clone() const{
+    return new LimitedCollectorVolunteer(getId(), getName(), getCoolDown(), maxOrders);
+}
+
+bool LimitedCollectorVolunteer::hasOrdersLeft() const{
+    return ordersLeft != 0;
+}
+
+/**
+ * returns true if volunteer has orders left, is not busy and orderStatus is PENDING
+*/
+bool LimitedCollectorVolunteer::canTakeOrder(const Order& order) const{
+    return hasOrdersLeft() && order.getStatus() == OrderStatus::PENDING && !isBusy();
+}
+
+/**
+ * accepts an order, updates timeLeft and activeOrderId
+ * decreases ordersLeft by 1 
+ * @param order - ref to a order the volunteer is going to handle
+*/
+void LimitedCollectorVolunteer::acceptOrder(const Order& order){
+    CollectorVolunteer::acceptOrder(order);
+    ordersLeft--;  
+}
+
+int LimitedCollectorVolunteer::getMaxOrders() const{
+    return maxOrders;
+}
+int LimitedCollectorVolunteer::getNumOrdersLeft() const{
+    return ordersLeft;
+}
+
+string LimitedCollectorVolunteer::toString() const{
+    string strTimeLeft =  getTimeLeft() == 0 ? "None" : std::to_string(timeLeft);
+    return "VolunteerID: " + std::to_string(getId()) + "\n"
+           +"isBusy: " + std::to_string(isBusy()) + "\n"
+           +"OrderID: " + std::to_string(activeOrderId) + "\n"
+           +"timeLeft: " + strTimeLeft + "\n"
+           +"ordersLeft: " + std::to_string(ordersLeft);
+}
