3b67b8984612a39ddea8c58a15fdbac2ecc758c3
eitandub22 <eitandu@post.bgu.ac.il>
2024-01-22 23:17:38 +0200

    added comments

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,7 +1,11 @@
 #include "include/Parser.h"
-
+//constructor
 Parser::Parser(){}
 
+/**
+ * @param configFilePath - the path to the configuration file, given by the user.
+ * @returns - vector of string containing each line of the file where each string is a line.
+*/
 vector<string> Parser::ParseFile(const string &configFilePath){
     fstream configFile;
     vector<string> data;
@@ -16,6 +20,10 @@ vector<string> Parser::ParseFile(const string &configFilePath){
     return data;
 }
 
+/**
+ * @param line - a line that we want to seperate to words.
+ * @return vector of strings where each string is a word in the line.
+ */
 vector<string> Parser::ParseLine(string line){
     string word;
     stringstream ss(line);
