6d5f90636b0922de56ba9893514697ab63fff822
FoyerD <amitfoye@post.bgu.ac.il>
2024-01-24 23:02:46 +0200

    finished PrintOrderStatus

---
+++ src/Action/PrintOrderStatus.cpp
@@ -0,0 +1,21 @@
+#include "Action.h"
+
+PrintOrderStatus::PrintOrderStatus(int id):
+                                    BaseAction::BaseAction(),
+                                    orderId(id){}
+PrintOrderStatus::~PrintOrderStatus(){}
+
+PrintOrderStatus* PrintOrderStatus::clone() const{
+    return new PrintOrderStatus(*this);
+}
+
+string PrintOrderStatus::toString() const{
+    return "orderStatus " + std::to_string(orderId);
+}
+
+
+void PrintOrderStatus::act(WareHouse& wareHouse){
+    Order& toPrint = wareHouse.getOrder(orderId);
+    if(toPrint.getId() == -1) error("Order doesn't exist");
+    std::cout << toPrint.printStatus();
+}
